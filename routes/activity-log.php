80447620be5e423163752f43f55eeb882ff9ee47
thanh <thanh@dcodegroup.com>
2024-01-17 15:30:48 +0700

    add test

--- routes/activity-log.php
+++ routes/activity-log.php
@@ -5,7 +5,7 @@ use Dcodegroup\ActivityLog\Controllers\API\FilterController;
 use Dcodegroup\ActivityLog\Controllers\API\ActivityLogController;
 use Illuminate\Support\Facades\Route;
 
-Route::get(config('activity-log.route_path'), ActivityLogController::class)->name(config('activity-log.route_name'));
-Route::post(config('activity-log.route_path') . '/comments', CommentController::class)->name(config('activity-log.route_name') . '.comment');
-Route::get(config('activity-log.route_name') . '/filters', FilterController::class)->name(config('activity-log.route_name') . '.filters');
-Route::get(config('activity-log.route_name') . '/filters/facets/{facet}', [FilterController::class, 'search'])->name(config('activity-log.route_name') . '.facets.search');
+Route::get('/'.config('activity-log.route_path'), ActivityLogController::class)->name(config('activity-log.route_name'));
+Route::post('/'.config('activity-log.route_path') . '/comments', CommentController::class)->name(config('activity-log.route_name') . '.comment');
+Route::get('/'.config('activity-log.route_name') . '/filters', FilterController::class)->name(config('activity-log.route_name') . '.filters');
+Route::get('/'.config('activity-log.route_name') . '/filters/facets/{facet}', [FilterController::class, 'search'])->name(config('activity-log.route_name') . '.facets.search');
