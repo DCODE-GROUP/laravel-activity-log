f9d06bc417310b048633aa7bf575e03c4e048949
hung-dcode <hung@dcodegroup.com>
2024-03-07 12:15:22 +0700

    fix: add comment url

--- resources/js/components/VActivityLog.vue
+++ resources/js/components/VActivityLog.vue
@@ -84,7 +84,7 @@ export default {
     },
     commentUrl: {
       type: String,
-      default: "api/generic/activity-logs/comments",
+      default: "/api/generic/activity-logs/comments",
     },
     modelClass: {
       type: String,
--- src/Resources/ActivityLog.php
+++ src/Resources/ActivityLog.php
@@ -15,7 +15,7 @@ class ActivityLog extends JsonResource
     {
         return [
             'id' => $this->resource->id,
-            'user' => $this->resource->user?->full_name,
+            'user' => $this->resource->user?->full_name ?: "Guest",
             'description' => $this->resource->description,
             'activitiable_type' => $this->resource->activitiable_type,
             'type' => $this->resource->type,
