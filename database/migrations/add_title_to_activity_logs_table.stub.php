aeb0cdb0e1335a127f089d39a266fac52f82e9ca
ntthanh1 <ntthanh1@users.noreply.github.com>
2024-03-14 14:54:58 +0000

    Fix PHP Syntax Styling

--- database/migrations/add_title_to_activity_logs_table.stub.php
+++ database/migrations/add_title_to_activity_logs_table.stub.php
@@ -1,7 +1,5 @@
 <?php
 
-use Dcodegroup\ActivityLog\Models\ActivityLog;
-use Dcodegroup\ActivityLog\Models\CommunicationLog;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
