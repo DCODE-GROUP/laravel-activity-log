0491ee23a2f0fbdbea54d3a69198f1173c65b13b
hungnt-dcode <hungnt-dcode@users.noreply.github.com>
2024-04-12 08:28:05 +0000

    Fix PHP Syntax Styling

--- src/Http/Controllers/API/CommentController.php
+++ src/Http/Controllers/API/CommentController.php
@@ -5,8 +5,8 @@ namespace Dcodegroup\ActivityLog\Http\Controllers\API;
 use Dcodegroup\ActivityLog\Http\Requests\ExistingRequest;
 use Dcodegroup\ActivityLog\Models\ActivityLog;
 use Dcodegroup\ActivityLog\Resources\ActivityLogCollection;
-use Illuminate\Routing\Controller;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Routing\Controller;
 
 class CommentController extends Controller
 {
@@ -32,12 +32,12 @@ class CommentController extends Controller
                 config('activity-log.user_relationship'),
                 $communication,
                 "$communication.reads",
-            ])->where(fn(Builder $builder) => $builder
-                ->whereNull('communication_log_id')
-                ->orWhere(fn(Builder $builder) => $builder
-                    ->whereNotNull('communication_log_id')
-                    ->whereNot('title', 'like', '% read an %')
-                    ->whereNot('title', 'like', '% view a %'))
+            ])->where(fn (Builder $builder) => $builder
+            ->whereNull('communication_log_id')
+            ->orWhere(fn (Builder $builder) => $builder
+                ->whereNotNull('communication_log_id')
+                ->whereNot('title', 'like', '% read an %')
+                ->whereNot('title', 'like', '% view a %'))
             )
             ->orderByDesc('created_at')->get());
     }
