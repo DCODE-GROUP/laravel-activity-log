d9dbf14fb6008aa06a046c45dbb853bb77b8eecf
hung-dcode <hung@dcodegroup.com>
2024-05-03 14:46:27 +0700

    feat: FLO-1240-in-some-projects-we-dont-have-a-username-field-in-the-users-table-instead-the-username-is-associat

--- config/activity-log.php
+++ config/activity-log.php
@@ -80,8 +80,9 @@ return [
     'user_relationship' => env('LARAVEL_ACTIVITY_LOG_USER_RELATIONSHIP', 'user'),
     'user_model' => \App\Models\User::class,
     'user_table' => env('LARAVEL_ACTIVITY_LOG_USERS_TABLE', 'users'),
-    'user_search_term' => env('LARAVEL_ACTIVITY_LOG_USER_SEARCH_TERM', 'email'),
+    'user_search_term' => env('LARAVEL_ACTIVITY_LOG_USER_SEARCH_TERM', ['email']),
     'user_search' => env('LARAVEL_ACTIVITY_LOG_USER_SEARCH', 'full_name'),
+    'user_search_relationship' => env('LARAVEL_ACTIVITY_LOG_USER_SEARCH_RELATIONSHIP', []),
 
     /*
      |--------------------------------------------------------------------------
