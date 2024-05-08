9a1b950a55a3f55dddbf765dec41396140a9c6f6
ntthanh1 <ntthanh1@users.noreply.github.com>
2024-05-08 07:22:58 +0000

    Fix eslint issues

--- resources/js/components/common/Comment.vue
+++ resources/js/components/common/Comment.vue
@@ -2,40 +2,38 @@
   <div class="content">
     <div class="content__text">
       <Mentionable
-          :keys="['@']"
-          :items="items"
-          offset="10"
-          filtering-disabled
-          insert-space
-          @open="loadUsers()"
-          @search="loadUsers($event)"
+        :keys="['@']"
+        :items="items"
+        offset="10"
+        filtering-disabled
+        insert-space
+        @open="loadUsers()"
+        @search="loadUsers($event)"
       >
         <textarea
-            @keyup.enter="addCommentByEnter"
-            class="content__text--textarea focus:ring-0"
-            v-model="comment"
-            rows="3"
-            :placeholder="$t('activity-log.placeholders.add_comment')"
+          @keyup.enter="addCommentByEnter"
+          class="content__text--textarea focus:ring-0"
+          v-model="comment"
+          rows="3"
+          :placeholder="$t('activity-log.placeholders.add_comment')"
         ></textarea>
         <template #no-result>
           <div class="dim">
             {{
               loading
-                  ? $t("activity-log.fields.loading")
-                  : $t("activity-log.fields.no_result")
+                ? $t("activity-log.fields.loading")
+                : $t("activity-log.fields.no_result")
             }}
           </div>
         </template>
 
         <template #item-@="{ item }">
           <div class="mention-wrapper">
-            <div
-                class="mention-wrapper--avatar activity__user--avatar"
-            >
+            <div class="mention-wrapper--avatar activity__user--avatar">
               <span class="font-bold !text-sm">{{
-                  item.label.charAt(0).toUpperCase() +
-                  item.label.charAt(1).toUpperCase()
-                }}</span>
+                item.label.charAt(0).toUpperCase() +
+                item.label.charAt(1).toUpperCase()
+              }}</span>
             </div>
             <span class="dim">
               {{ item.label }}
@@ -48,9 +46,9 @@
     <div class="content__action">
       <div class="content__action-attachment"></div>
       <div
-          class="content__action-button cursor-pointer"
-          :class="{ 'content__action-button--disable': !comment }"
-          @click="addComment"
+        class="content__action-button cursor-pointer"
+        :class="{ 'content__action-button--disable': !comment }"
+        @click="addComment"
       >
         {{ $t("activity-log.buttons.save_comment") }}
       </div>
@@ -104,19 +102,17 @@ export default {
     async loadUsers(searchText = null) {
       this.loading = true;
       axios
-          .get(
-              `${this.loadUsersUrl}/created_by?s=${searchText}`,
-          )
-          .then((res) => {
-            this.items = res.data.map((item) => {
-              return {
-                label: item.label,
-                value: `[${item.label}]`,
-                id: item.value,
-              };
-            });
-            this.loading = false;
+        .get(`${this.loadUsersUrl}/created_by?s=${searchText}`)
+        .then((res) => {
+          this.items = res.data.map((item) => {
+            return {
+              label: item.label,
+              value: `[${item.label}]`,
+              id: item.value,
+            };
           });
+          this.loading = false;
+        });
     },
     addCommentByEnter() {
       if (this.enterToComment) {
@@ -128,18 +124,18 @@ export default {
         return;
       }
       axios
-          .post(this.commentUrl, {
-            modelClass: this.modelClass,
-            modelId: this.modelId,
-            comment: this.comment,
-            currentUrl: window.location.href,
-            currentUser: this.user,
-          })
-          .then(({ data }) => {
-            this.comment = null;
-            this.$emit("addComment", data.data);
-          })
-          .catch(console.error);
+        .post(this.commentUrl, {
+          modelClass: this.modelClass,
+          modelId: this.modelId,
+          comment: this.comment,
+          currentUrl: window.location.href,
+          currentUser: this.user,
+        })
+        .then(({ data }) => {
+          this.comment = null;
+          this.$emit("addComment", data.data);
+        })
+        .catch(console.error);
     },
   },
 };
