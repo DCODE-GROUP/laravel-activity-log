932136c9641df36dc23db2f84d778029c6f97922
Nathan Rzepecki <nathan@lionslair.net.au>
2024-06-06 15:19:23 +0800

    Update the default name

--- src/Support/Traits/ActivityLoggable.php
+++ src/Support/Traits/ActivityLoggable.php
@@ -60,20 +60,7 @@ trait ActivityLoggable
 
     public function activityLogEntityName(): string
     {
-
-        if (isset($this->name)) {
-            return $this->name;
-        }
-
-        if (isset($this->title)) {
-            return $this->title;
-        }
-
-        if (isset($this->label)) {
-            return $this->label;
-        }
-
-        return $this->id;
+        return Str::title(class_basename($this));
     }
 
     public function logUpdate(): void
