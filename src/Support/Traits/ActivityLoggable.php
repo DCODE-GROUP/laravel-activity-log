1e91ff5f518f71f97a2a2b9c1db1af2be1f5912c
Vu Ngoc Tung <tungvn.0427@gmail.com>
2024-07-11 10:14:07 +0700

    refactor: separate methods for non activity loggable models

--- src/Support/Traits/ActivityLoggable.php
+++ src/Support/Traits/ActivityLoggable.php
@@ -3,7 +3,6 @@
 namespace Dcodegroup\ActivityLog\Support\Traits;
 
 use Coduo\ToString\StringConverter;
-use Dcodegroup\ActivityLog\Exceptions\ModelKeyNotDefinedException;
 use Dcodegroup\ActivityLog\Models\ActivityLog;
 use Dcodegroup\ActivityLog\Models\CommunicationLog;
 use Illuminate\Database\Eloquent\Model;
@@ -12,13 +11,14 @@ use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Mail\Mailables\Address;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Collection;
-use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Str;
 use ReflectionClass;
 use ReflectionMethod;
 
 trait ActivityLoggable
 {
+    use ModelDeterminable;
+
     public static function bootActivityLoggable()
     {
         static::created(function ($model) {
@@ -191,55 +191,6 @@ trait ActivityLoggable
         }
 
         return $relationships;
-    }
-
-    public function determineModelKey(): string
-    {
-        /**
-         * Check if the label has been set in the model
-         */
-        if (! empty($this->getActivityLogModelKey())) {
-            return $this->getActivityLogModelKey();
-        }
-
-        $standardKeys = ['name', 'title', 'label'];
-
-        foreach ($standardKeys as $key) {
-            if (collect($this->getAttributes())->has($key)) {
-                return $this->{$key};
-            }
-        }
-
-        throw new ModelKeyNotDefinedException(__('activity-log.exceptions.model_key', ['model' => class_basename($this)]));
-    }
-
-    public function getActivityLogModelKey(): string
-    {
-        return '';
-    }
-
-    public function determineModelLabel(): string
-    {
-        /**
-         * check if we have the model label in cache
-         */
-        if (Cache::has('model_label_'.class_basename($this))) {
-            return Cache::get('model_label_'.class_basename($this));
-        }
-
-        /**
-         * Check if the label has been set in the model
-         */
-        if (! empty($this->getActivityLogModelLabel())) {
-            return Cache::rememberForever('model_label_'.class_basename($this), fn () => $this->getActivityLogModelLabel());
-        }
-
-        return Cache::rememberForever('model_label_'.class_basename($this), fn () => Str::headline(class_basename($this)));
-    }
-
-    public function getActivityLogModelLabel(): string
-    {
-        return '';
     }
 
     public function getModelChanges(?array $modelChangesJson = null): string
