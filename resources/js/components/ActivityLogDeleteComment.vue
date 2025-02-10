6ae46f65a2eb6454e30ff3846d18675edc99ab1b
Nathan Rzepecki <nathan@lionslair.net.au>
2025-02-10 13:56:54 +0800

    wip

--- resources/js/components/ActivityLogDeleteComment.vue
+++ resources/js/components/ActivityLogDeleteComment.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-col flex-1">
     <div class="flex flex-col gap-4 flex-1">
       <div class="p-4 flex-1 flex flex-col gap-2">
-        {{ $t("activity-log.headings.confirm_delete") }}
+        {{ $t("activity-log.headings.confirm_delete_comment") }}
       </div>
 
       <footer
