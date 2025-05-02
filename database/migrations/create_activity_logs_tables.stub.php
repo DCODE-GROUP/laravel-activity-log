dc8fafb21fd74076dd0238e4d9c87af6aa3294a2
Nathan Rzepecki <nathan@lionslair.net.au>
2025-05-02 09:48:11 +0800

    cleanup migration

--- database/migrations/create_activity_logs_tables.stub.php
+++ database/migrations/create_activity_logs_tables.stub.php
@@ -8,12 +8,7 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
+    public function up(): void
     {
         Schema::create('communication_logs', function (Blueprint $table) {
             $table->id();
@@ -45,12 +40,7 @@ return new class extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('activity_logs');
         Schema::dropIfExists('communication_logs');
