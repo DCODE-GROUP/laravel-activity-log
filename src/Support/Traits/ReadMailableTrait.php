08cf716244ff4b7b59347f89368a03aefdd0a72a
Nathan Rzepecki <nathan@lionslair.net.au>
2025-05-16 10:50:58 +0800

    wip

--- src/Support/Traits/ReadMailableTrait.php
+++ src/Support/Traits/ReadMailableTrait.php
@@ -3,6 +3,7 @@
 namespace Dcodegroup\ActivityLog\Support\Traits;
 
 use Dcodegroup\ActivityLog\Models\ActivityLog;
+use Illuminate\Container\Container;
 use Illuminate\Mail\Mailables\Content;
 
 trait ReadMailableTrait
@@ -14,9 +15,81 @@ trait ReadMailableTrait
 
     protected $model = null;
 
-    public function prepareContent(): Content
+    public function send($mailer)
     {
-        return new Content;
+        // todo: this code to skip error on phpunit test
+        if (app()->environment('testing')) {
+            return parent::send($mailer);
+        }
+
+        if (method_exists($this->model, 'createCommunicationLog') && method_exists($this->model, 'createActivityLog')) {
+            $envelope = $this->envelope();
+            $to = $this->to[0]['address'];
+            $communicationLog = $this->model->createCommunicationLog([
+                'subject' => $envelope->subject,
+                'cc' => $envelope->cc,
+                'bcc' => $envelope->bcc,
+            ], $to, $this->logRender() ?: '');
+            $this->activityLog = $this->model->createActivityLog([
+                'type' => ActivityLog::TYPE_NOTIFICATION,
+                'title' => __('activity-log.words.send_email').$to,
+                'description' => '',
+                'communication_log_id' => $communicationLog->id,
+            ]);
+        }
+
+        return parent::send($mailer);
+    }
+
+    private function logRender()
+    {
+        $logMailable = clone $this;
+
+        return $logMailable->withLocale($logMailable->locale, function () use ($logMailable) {
+            if (method_exists($logMailable, 'build')) {
+                Container::getInstance()->call([$logMailable, 'build']);
+            }
+
+            //            $this->activityLogEnsureContentIsHydrated($logMailable);
+
+            return Container::getInstance()->make('mailer')->render(
+                $logMailable->buildView(), $logMailable->buildViewData()
+            );
+        });
+
+    }
+
+    private function activityLogEnsureContentIsHydrated($logMailable)
+    {
+        if (! method_exists($logMailable, 'content')) {
+            return;
+        }
+
+        $content = $logMailable->content();
+
+        if ($content->view) {
+            $this->view($content->view);
+        }
+
+        if ($content->html) {
+            $this->view($content->html);
+        }
+
+        if ($content->text) {
+            $this->text($content->text);
+        }
+
+        if ($content->markdown) {
+            $this->markdown($content->markdown);
+        }
+
+        if ($content->htmlString) {
+            $this->html($content->htmlString);
+        }
+
+        foreach ($content->with as $key => $value) {
+            $this->with($key, $value);
+        }
     }
 
     /**
@@ -34,29 +107,8 @@ trait ReadMailableTrait
         return $content;
     }
 
-    public function send($mailer)
+    public function prepareContent(): Content
     {
-        // todo: this code to skip error on phpunit test
-        if (app()->environment('testing')) {
-            return parent::send($mailer);
-        }
-
-        if (method_exists($this->model, 'createCommunicationLog') && method_exists($this->model, 'createActivityLog')) {
-            $envelope = $this->envelope();
-            $to = $this->to[0]['address'];
-            $communicationLog = $this->model->createCommunicationLog([
-                'subject' => $envelope->subject,
-                'cc' => $envelope->cc,
-                'bcc' => $envelope->bcc,
-            ], $to, $this->render() ?: '');
-            $this->activityLog = $this->model->createActivityLog([
-                'type' => ActivityLog::TYPE_NOTIFICATION,
-                'title' => __('activity-log.words.send_email').$to,
-                'description' => '',
-                'communication_log_id' => $communicationLog->id,
-            ]);
-        }
-
-        return parent::send($mailer);
+        return new Content();
     }
 }
