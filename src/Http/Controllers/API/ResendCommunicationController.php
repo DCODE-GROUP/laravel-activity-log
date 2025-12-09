e34bb662388fc6aa2b1170c45ed1324cd32b825f
hungnt-dcode <148025212+hungnt-dcode@users.noreply.github.com>
2025-12-09 09:47:25 +0000

    Fix PHP Syntax Styling

--- src/Http/Controllers/API/ResendCommunicationController.php
+++ src/Http/Controllers/API/ResendCommunicationController.php
@@ -3,10 +3,6 @@
 namespace Dcodegroup\ActivityLog\Http\Controllers\API;
 
 use App\Mail\CustomHtmlMail;
-use Dcodegroup\ActivityLog\Events\ActivityLogCommentCreated;
-use Dcodegroup\ActivityLog\Http\Requests\ExistingRequest;
-use Dcodegroup\ActivityLog\Http\Services\ActivityLogService;
-use Dcodegroup\ActivityLog\Models\ActivityLog;
 use Dcodegroup\ActivityLog\Models\CommunicationLog;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Mail;
