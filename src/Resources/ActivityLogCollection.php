3dd4e5bb4901ebf3752f43e7330ea49912a4f624
thanh <thanh@dcodegroup.com>
2026-07-19 15:43:47 +0700

    Use ActivityLog resource for collection so reactions and groups are returned to client

--- src/Resources/ActivityLogCollection.php
+++ src/Resources/ActivityLogCollection.php
@@ -4,9 +4,15 @@ namespace Dcodegroup\ActivityLog\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\ResourceCollection;
+use Dcodegroup\ActivityLog\Resources\ActivityLog as ActivityLogResource;
 
 class ActivityLogCollection extends ResourceCollection
 {
+    /**
+     * Ensure each item is transformed using ActivityLog resource
+     */
+    public $collects = ActivityLogResource::class;
+
     /**
      * Transform the resource collection into an array.
      *
